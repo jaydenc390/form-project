1c9bc1b9cb941e7edf7bcd529c47313943b4f178
Jayden Chen <jaydenc390@nycstudents.net>
2026-03-03 10:59:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 let form = document.querySelector("form");
-let submit = document.querySelector("button");
-
+let button = document.querySelector("button");
+const inputElements = form.querySelectorAll('input, textarea');
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+});
+
+button.addEventListener("click", function() {
+  alert("Form Submission Successful!");
+  document.querySelector("form").reset();
+  
+      let allFieldsFilled = true;
+  
+      inputElements.forEach(input => {
+        if (input.value.trim() === '') {
+            allFieldsFilled = false;
+        }
+    });
+      if (allFieldsFilled) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+});
+
+form.addEventListener('input', function () {
+    let allFieldsFilled = true;
+  
+      inputElements.forEach(input => {
+        if (input.value.trim() === '') {
+            allFieldsFilled = false;
+        }
+    });
+
+    if (allFieldsFilled) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
 });
